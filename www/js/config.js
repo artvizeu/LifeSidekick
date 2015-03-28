835bd12ac80b5faf85d8d93e255889faa9d373c0
mbelev <martinbelev93@gmail.com>
2015-03-28 15:58:13 +0200

    Change app and javascript id.

--- www/js/config.js
+++ www/js/config.js
@@ -3,7 +3,7 @@
 lifeSidekickApp
     .factory("config", function () {
         return {
-            APP_ID: 'lxAsQ1JgMWELK3HRFTUDnvkptQZThWmecgf7LiZX',
-            JAVASCRIPT_ID: 'Vga3cqKhuQ3opVoYFqiktGW3UYf61ioNBWyROKgi'
+            APP_ID: 'zDZfhWeKhE8gn3jAVJkSmNk95Bq395jpI6ZPgj9s',
+            JAVASCRIPT_ID: '3bRL5av4Fs4LxljF6bNilbYCSNIUoB6TYP5vJUzu'
         }
     });
